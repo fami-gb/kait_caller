91817ecc1d946aabae1a3ee3c0ad421219cc8994
fami-gb <fami.gb0825@gmail.com>
2024-10-23 04:36:57 +0900

    feat: オウム返しを実装

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import {
+  ClientConfig,
+  Client,
+  middleware,
+  MiddlewareConfig,
+  WebhookEvent,
+  TextMessage,
+  MessageAPIResponseBase,
+} from '@line/bot-sdk';
+import express, { Application, Request, Response } from 'express';
+import { load } from 'ts-dotenv';
+const env = load({
+  CHANNEL_ACCESS_TOKEN: String,
+  CHANNEL_SECRET: String,
+  PORT: Number,
+});
+
+const PORT = env.PORT || 3000;
+
+const config = {
+  channelAccessToken: env.CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: env.CHANNEL_SECRET || '',
+};
+const clientConfig: ClientConfig = config;
+const middlewareConfig: MiddlewareConfig = config;
+const client = new Client(clientConfig); //①
+
+const app: Application = express(); //②
+
+app.get('/', async (_: Request, res: Response): Promise<void> => { //③
+  res.status(200).send({
+    message: 'success',
+  });
+});
+
+const textEventHandler = async ( //④
+  event: WebhookEvent
+): Promise<MessageAPIResponseBase | undefined> => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return;
+  }
+
+
+  const { replyToken } = event;
+  const { text } = event.message;
+  const response: TextMessage = {
+    type: 'text',
+    text: text,
+  };
+  await client.replyMessage(replyToken, response);
+};
+
+app.post( //⑤
+  '/webhook',
+  middleware(middlewareConfig),
+  async (req: Request, res: Response): Promise<void> => {
+    const events: WebhookEvent[] = req.body.events;
+    await Promise.all(
+      events.map(async (event: WebhookEvent) => {
+        try {
+          await textEventHandler(event);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            console.error(err);
+          }
+          res.status(500);
+        }
+      })
+    );
+    res.status(200);
+  }
+);
+
+app.listen(PORT, () => { //⑥
+  console.log(`http://localhost:${PORT}/`);
+});
